157ea8a3f5796a2cec2dc3124e30cd935dd35228
MohamedAdel <adel762dp47@gmail.com>
2023-11-01 14:41:26 -0700

    message

---
+++ sm.h
@@ -0,0 +1,16 @@
+#ifndef SM_H
+#define SM_H
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <linux/limits.h>
+extern char **environ;
+
+#endif
