02f61e99b7d5cc3dd56e4fff56df90b249421a77
MohamedAdel <adel762dp47@gmail.com>
2023-11-01 14:52:59 -0700

    message

---
+++ simple_shell.c
@@ -0,0 +1,32 @@
+#include "sm.h"
+
+/**
+ * main - simple_shell program
+ * @ac: int input 1
+ * @av: char input 2
+ *
+ * Return: int var
+*/
+int main(int ac, char **av)
+{
+	char *conn;
+	char pro[] = "$ ";
+
+	do {
+		conn = malloc(ml * (ac / ac));
+		if (isatty(STDIN_FILENO) == 1)
+			SM_string(pro);
+		if (fgets(conn, ml, stdin) == NULL)
+		{
+			if (isatty(STDIN_FILENO) == 1)
+				SM_string("\n");
+			free(conn);
+			break;
+		}
+		com[strcspn(conn, "\n")] = '\0';
+		_ex(environ, &conn, av);
+		free(conn);
+	} while (1);
+return (0);
+}
+
